108531ab8ec467483b09cd1171860c66b5f948e3
Aasif Nawaz <aasiffiraz178@gmail.com>
2021-12-05 23:05:40 +0530

    Created New Activity for Appoinments

---
+++ app/src/main/java/com/aasif/cl_hdcse_95_46/Appoinment_Booking.java
@@ -0,0 +1,14 @@
+package com.aasif.cl_hdcse_95_46;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Appoinment_Booking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appoinment_booking);
+    }
+}
