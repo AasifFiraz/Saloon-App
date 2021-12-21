1db46b74751e65ab27f1095c29ca0a4553ff67df
Aasif Nawaz <aasiffiraz178@gmail.com>
2021-12-21 23:09:55 +0530

    Added alert dialog for admin

--- app/src/main/java/com/aasif/cl_hdcse_95_46/AdminAppointments.java
+++ app/src/main/java/com/aasif/cl_hdcse_95_46/AdminAppointments.java
@@ -2,8 +2,17 @@ package com.aasif.cl_hdcse_95_46;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
@@ -13,6 +22,10 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class AdminAppointments extends AppCompatActivity {
+    ListView lvAppointments;
+    TextView txtNoAppointment;
+    Button btnEditAppointment, btnDeleteAppointment;
+    ViewGroup viewGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,21 +33,111 @@ public class AdminAppointments extends AppCompatActivity {
         setContentView(R.layout.activity_admin_appointments);
         getSupportActionBar().hide();
 
-        ListView lv = findViewById(R.id.lstAllAppointments);
-        TextView txtNoAppointment = findViewById(R.id.txtNoAppointmentMessage);
+        lvAppointments = findViewById(R.id.lstAllAppointments);
+        txtNoAppointment = findViewById(R.id.txtNoAppointmentMessageAdmin);
+        viewGroup = findViewById(R.id.content);
 
         DBConnector dbcon = new DBConnector(this);
-        ArrayList<HashMap<String, String>> userList = dbcon.GetAllAppointments();
+        showAppointmentList(dbcon);
+
+        lvAppointments.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(AdminAppointments.this);
+                View view1 = LayoutInflater.from(AdminAppointments.this).inflate(R.layout.custom_alert_dialog_appointment, viewGroup, false);
+                builder.setCancelable(true);
+                builder.setView(view1);
+
+                final AlertDialog alertDialog = builder.create();
+                btnDeleteAppointment = view1.findViewById(R.id.btnDeleteAppointment);
+                btnEditAppointment = view1.findViewById(R.id.btnEditAppointment);
+                alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+                ArrayList<HashMap<String, String>> userList = dbcon.GetAppointmentsForCustomer();
+
+                ListAdapter adapter = new SimpleAdapter(AdminAppointments.this, userList, R.layout.appointments_list,
+                        new String[]{"name", "date", "price"}, new int[]{R.id.txtBookedName, R.id.txtBookedDate,
+                        R.id.txtBookedPrice});
+
+
+                HashMap<String, Object> obj = (HashMap<String, Object>) adapter.getItem(position);
+
+                btnDeleteAppointment.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        String appoinmentId = (String) obj.get("id");
+                        dbcon.deleteAppointment(appoinmentId);
+                        showAppointmentList(dbcon);
+
+                        alertDialog.dismiss();
+                    }
+                });
+
+                btnEditAppointment.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String appointmentId = (String) obj.get("id");
+                        String appointmentName = (String) obj.get("name");
+                        String appointmentPrice = (String) obj.get("price");
+                        String appointmentDateTime = (String) obj.get("date");
+                        String [] sepAppointmentDateTime = appointmentDateTime.split(" ");
+
+                        Bundle bundle = new Bundle();
+                        Intent appointment_edit_page = new Intent(AdminAppointments.this, Appointment_Edit_Activity_Admin.class);
+                        bundle.putString("appointId",appointmentId);
+                        bundle.putString("appointName", appointmentName);
+                        bundle.putString("appointPrice",appointmentPrice);
+                        bundle.putString("appointDate",sepAppointmentDateTime[0]);
+                        bundle.putString("appointTime",sepAppointmentDateTime[1]);
+                        appointment_edit_page.putExtras(bundle);
+                        startActivity(appointment_edit_page);
+                        alertDialog.dismiss();
+                    }
+                });
+
+                alertDialog.show();
+
+            }
+        });
+    }
+    private void showAppointmentList (DBConnector dbcon){
+        ArrayList<HashMap<String, String>> userList = dbcon.GetAppointmentsForCustomer();
 
         ListAdapter adapter = new SimpleAdapter(this, userList, R.layout.appointments_list,
-                new String[]{"name","date","time","price"}, new int[]{R.id.txtBookedName, R.id.txtBookedDate,
-                R.id.txtBookedTime,R.id.txtBookedPrice});
+                new String[]{"name","date","price"}, new int[]{R.id.txtBookedName, R.id.txtBookedDate,
+                R.id.txtBookedPrice});
 
-        if (adapter.getCount() < 0)
-        {
-            txtNoAppointment.setVisibility(View.VISIBLE);
-        }else{
-            lv.setAdapter(adapter);
-        }
+
+        lvAppointments.setEmptyView(txtNoAppointment);
+        lvAppointments.setAdapter(adapter);
+    }
+
+    @Override
+    public void onBackPressed() {
+        androidx.appcompat.app.AlertDialog.Builder builder1 = new androidx.appcompat.app.AlertDialog.Builder(this);
+        builder1.setMessage("Do You Want to Logout?");
+        builder1.setCancelable(true);
+
+        builder1.setPositiveButton(
+                "Yes",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Intent i = new Intent(getApplicationContext(), Main_Screen.class);
+                        startActivity(i);
+                    }
+                });
+
+        builder1.setNegativeButton(
+                "No",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        androidx.appcompat.app.AlertDialog alert11 = builder1.create();
+        alert11.show();
     }
 }
